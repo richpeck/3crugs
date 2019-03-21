b628ecf53d5cb7379b7553d2472413d6077b16bc
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-03-21 09:27:08 +0000

    Release.sh

--- vendor/gems/fl/app/controllers/application_controller.rb
+++ vendor/gems/fl/app/controllers/application_controller.rb
@@ -25,40 +25,6 @@ class ApplicationController < ActionController::Base
   ##################################
   ##################################
 
-    # => Specific page content
-    # => All slugrouter requests passed here (including root)
-    # => Liquid ref - https://github.com/Shopify/liquid/wiki/Liquid-for-Programmers#first-steps
-    # => If not using bang operator in find, use || "No Content"
-    def show
-      raise ActionController::RoutingError.new('Not Found') if params[:id] == "index"
-      @content = Meta::Page.find_by_slug! params[:id] || "index"
-    end
-
-  ##################################
-  # => Auth
-  ##################################
-
-    # => New
-    def new
-      @content = Node.new
-      respond_with @content
-    end
-
-    # => Create
-    def create
-      @content = Node.new node_params
-      respond_with @content, location: -> { application_path(@content) }
-    end
-
-    # => Update
-    def update
-      @content = Node.find_by_slug! params[:id]
-      respond_with @content, location: -> { application_path(@content) }
-    end
-
-  ##################################
-  ##################################
-
     # => Robots.txt
     # => http://api.rubyonrails.org/classes/ActionController/ConditionalGet.html#method-i-expires_in
     def robots
