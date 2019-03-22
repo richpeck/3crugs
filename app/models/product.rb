6e04f5dff12c8bc2e1b476ca319453d1c8977c5c
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-03-22 15:03:45 +0000

    Sync API

--- app/models/product.rb
+++ app/models/product.rb
@@ -30,7 +30,29 @@ class Product < ApplicationRecord
   alias_attribute :product_code, :vad_variant_code
 
   # => Sync
+  # => Uses Savon gem to communicate with EKM API
+  # => All we're going to do is ping the API with the ProductCode and update the Stock number
+  # => https://engineering.tripping.com/savon-soap-in-ruby-6e6fce06537b
   def sync!
+
+    # => Vars
+    api = Meta::Option.find_by(ref: "app_ekm").val
+
+    # => Savon
+    # => This allows us to send the request
+    client = Savon.client(wsdl: "http://publicapi.15.ekm.net/v1.1/publicapi.asmx?WSDL")
+
+    puts client.operations
+
+    # => To raise errors
+    begin
+      client.call(:set_product_stock, message: { APIKey: api, ProductCode: vad_variant_code, ProductStock: free_stock }, convert_request_keys_to: :camelcase)
+    rescue Savon::Error => error
+      Rails.logger.info error.inspect()
+      raise
+    end
+
+    # => Update the db
     update_attributes synced_at: DateTime.now
   end
 
