1028242b9dd2f34f1e30c1a729da21675fbdf365
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-03-27 14:59:34 +0000

    Queue

--- app/jobs/sync_job.rb
+++ app/jobs/sync_job.rb
@@ -22,7 +22,7 @@ class SyncJob < ActiveJob::Base
   ## Too Many Requests ##
   ## Rescues the update and resubmits to the end of the queue ##
   rescue_from(StandardError) do
-    retry_job queue: :low_priority
+    retry_job queue: :sync
   end
 
   ## After Perform ##
